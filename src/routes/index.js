709639c682f17840d689a944a11149ff9730e703
fewcatltd <137312580+fewcatltd@users.noreply.github.com>
2024-10-27 19:25:31 +0400

    Small fix

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,6 @@ import imageRouter from './image.js';
 import healthRouter from './health.js';
 import createRateLimitMiddleware from '../middlewares/rateLimiterMiddleware.js';
 
-import Logger from '../common/logger.js';
-
 export default (app, redis) => {
   app.use(
     '/images',
@@ -34,11 +32,6 @@ export default (app, redis) => {
       windowMs: 5 * 1000,
       max: 1,
       message: 'Too many requests, please try again later.',
-      skip: (req) => {
-        Logger.child({module: 'health rate limit'}).info('Request IP:', req.ip);
-        const allowList = ['127.0.0.1', '::1', 'kubernetes-ip'];
-        return allowList.includes(req.ip);
-      },
       redis,
     }),
     healthRouter
